8bc5da31f483bd9785a24bc3f27a6dc2ad5ff8c1
JMUR <architect.lock@outlook.com>
2023-11-27 16:26:36 +0300

    convert tasks execution from concurrency to consistently

--- bot/parsing/tracking.py
+++ bot/parsing/tracking.py
@@ -1,3 +1,4 @@
+from loguru import logger
 import asyncio
 from parsing.parser import sync_avito
 from telegram.initializer import dp
@@ -5,7 +6,7 @@ from telegram.keyboards import item_inline_kb
 from db import DBCommands
 
 
-async def form_answer(user_id: int, task: dict, name: str) -> None:
+async def form_answer(user_id: int, task: dict, name: str):
     markup = await item_inline_kb(task.get('link'))
     text = f"Обновление для {name}!\n\n" \
            f"Название: {task.get('name', '')}\n\n" \
@@ -13,26 +14,26 @@ async def form_answer(user_id: int, task: dict, name: str) -> None:
            f"Описание: {task.get('description', '')}\n\n"
     image = task.get('img', None)
     if image:
-        await dp.bot.send_photo(
+        return await dp.bot.send_photo(
             chat_id=user_id,
             photo=f"{image}",
             caption=text,
             reply_markup=markup
         )
     else:
-        await dp.bot.send_message(
+        return await dp.bot.send_message(
             chat_id=user_id,
             text=text,
             reply_markup=markup,
         )
 
 
-async def is_trackable(user_id: int, workers: dict) -> bool:
+async def is_trackable(user_id: int, tasks_quantity: int) -> bool:
     db = DBCommands()
     available_workers = 1
     if await db.is_subscriber(user_id):
         available_workers = await db.worker_quantity_check(user_id)
-    if len(workers.keys()) > available_workers:
+    if tasks_quantity > available_workers:
         return False
     if await db.is_alive(user_id) == 0:
         return False
@@ -40,34 +41,32 @@ async def is_trackable(user_id: int, workers: dict) -> bool:
 
 
 async def start_tracking():
+    """
+    Функция забирает активные задачи для каждого пользователя
+    и формирует из них Future объект для ПОСЛЕДОВАТЕЛЬНОГО выполнения
+    Это обусловлено возможностью получить бан пой IP на авито или капчу
+    """
     db = DBCommands()
     while True:
-        # Получаем всех активных юзеров и валидируем
         active_users = await db.get_active_users()
         for user_id in list(active_users):
-            worker = await db.read_tasks(user_id)
-            if not await is_trackable(user_id, worker):
+            tasks = await db.read_tasks(user_id)
+            if not await is_trackable(user_id=user_id, tasks_quantity=len(tasks.keys())):
                 continue
-            # Забираем названия задач и необходимые URL
-            urls, tasks_names = list(worker.values()), list(worker.keys())
-            # Формируем из них Future объект для конкурентного выполнения
-            tasks = [
-                asyncio.create_task(
-                    asyncio.to_thread(sync_avito, url)) for url in urls
-            ]
-            now = dict(zip(tasks_names, await asyncio.gather(*tasks)))
-            for task_name in tasks_names:
-                task = now.get(task_name)
+            for task_name, task_url in tasks.items():
+                result = await asyncio.create_task(
+                    asyncio.to_thread(sync_avito, task_url)
+                )
                 first_result = await db.read_result(user_id, task_name)
-                if task is not None and task.get('name') not in first_result:
+                if result is not None and result.get('name') not in first_result:
                     await db.register_first_result(
                         user_id,
                         task_name,
-                        task.get('name')
+                        result.get('name')
                     )
                     await form_answer(
                         user_id=user_id,
-                        task=task,
+                        task=result,
                         name=task_name
                     )
-                await asyncio.sleep(20)
+                await asyncio.sleep(10)
